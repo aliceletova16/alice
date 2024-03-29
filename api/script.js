d375727eefc37deb78ad637c6565e6cf97df6e0c
aliceletova16 <151494778+aliceletova16@users.noreply.github.com>
2024-03-29 11:30:31 +0500

    Add files via upload

---
+++ api/script.js
@@ -0,0 +1,36 @@
+const wrapper = document.querySelector('.wrapper')
+const loader = document.querySelector('.loader')
+
+
+const func = async () => {
+    const response = await fetch('https://dummyjson.com/users')
+
+    const details = await response.json()
+
+    console.log(details.users);
+
+
+    details.users.map((el) => {
+        wrapper.innerHTML += `
+        <div class="card">
+        <img src=${el.image} alt="">
+        <h2>${el.firstName} ${el.lastName}</h2>
+        <h3></h3>
+        <p>${el.age}</p>
+        <b>${el.birthDate}</b>
+        <h4>${el.address.city}</h4>
+    </div>
+        `
+    })
+}
+
+func()
+
+setTimeout(() => {
+    loader.remove()
+}, 2000);
+
+
+
+
+
